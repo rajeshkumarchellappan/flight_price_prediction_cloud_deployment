7288694adcd015aca324f4344a857818473a2e04
rajeshkumarchellappan <75208375+rajeshkumarchellappan@users.noreply.github.com>
2021-08-24 18:02:37 +0530

    Add files via upload

---
+++ model.py
@@ -0,0 +1,37 @@
+import pandas as pd
+import numpy as np
+import sklearn
+from sklearn.ensemble import  ExtraTreesRegressor
+#import xgboost as xgb
+
+from sklearn.model_selection import train_test_split
+
+
+
+df=pd.read_csv('flight_pred_df')
+df.drop('Unnamed: 0',axis=1,inplace=True)
+df.drop('Additional_Info',axis=1,inplace=True)
+
+
+#print(df.head())
+X=df.drop('Price',axis=1)
+y=df['Price']
+print(X.columns)
+X_train,X_test,y_train,y_test=train_test_split(X,y,test_size=.3,random_state=20)
+
+
+#Preparing ExtraTrees Regression
+extr_model=ExtraTreesRegressor()
+
+extr_model.fit(X_train,y_train)
+y_pred_extr=extr_model.predict(X_test)
+print(len(X_test.columns))
+print(len(X_test.columns))
+
+
+
+import pickle
+# # Saving model to disk
+pickle.dump(extr_model, open('model.pkl','wb'))
+model=pickle.load(open('model.pkl','rb'))
+print(y_pred_extr)
